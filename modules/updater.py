29e43522ecae24105064dcfebda42e8200d3b366
GrzegorzKrug <blank>
2023-08-03 12:34:43 +0200

    faster updater

--- modules/updater.py
+++ modules/updater.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
         fp.write("\n")
 
     with open(output_filepath, "at", buffering=1)as fp:
-        for i in range(len(dataframe)):
-            time.sleep(np.random.random() + 0.05)
+        for i in range(104300, len(dataframe)):
+            time.sleep(np.random.random() * 0.05)
             ser = dataframe.iloc[i, :]
             fp.write(','.join(map(str, ser)))
             fp.write("\n")
