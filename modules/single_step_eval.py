35701f49f8369552a93c5f344c1a5109a2984518
GrzegorzKrug <blank>
2023-08-09 19:23:15 +0200

    fix path

--- modules/single_step_eval.py
+++ modules/single_step_eval.py
@@ -819,7 +819,7 @@ if __name__ == "__main__":
     # input_filepath = os.path.join(path_data_folder, "test_updating.txt")  # Local file
 
     "MACHINE STARTS"
-    start_stream_predict(input_filepath, time_window=50, time_feats=17, nodes=1000 ,arch_num=101, loss='mae', lr='1e-06', output_filepath="/home/rafal/predict_1_huber_1e-06.txt", iteration=0,)
+    start_stream_predict(input_filepath, time_window=50, time_feats=17, nodes=1000 ,arch_num=101, loss='mae', lr='1e-06', output_filepath="/home/rafal/predict_101_mae_1e-06.txt", iteration=0,)
     # # start_stream_predict(input_filepath, arch_num=1, loss='huber', lr='1e-06', output_filepath="")
     # # start_stream_predict(input_filepath, arch_num=103, loss='mae', lr='1e-06', output_filepath="/home/rafal/predict_103_mae_1e-06.txt")
     # # start_stream_predict(input_filepath, arch_num=101, loss='mae', lr='1e-05', output_filepath="/home/rafal/predict_101_mae_1e-05.txt")
